534b2b2e2cb604b895d807fedeb23efd1bd8a2cd
Pritish Yuvraj <pritish.yuvi@gmail.com>
2017-12-25 03:05:35 -0500

    Fully parses JSON File

--- JSON Parser/app/src/main/java/com/example/priti/live/MainActivity.java
+++ JSON Parser/app/src/main/java/com/example/priti/live/MainActivity.java
@@ -4,18 +4,24 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.w3c.dom.Text;
 import android.util.Log;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,14 +30,18 @@ public class MainActivity extends AppCompatActivity {
     private String TAG = MainActivity.class.getSimpleName();
     private int userid;
     private String title;
+    ArrayList<HashMap<String, String>> database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = new ArrayList<>();
         mSearchResultsTextView = (TextView) findViewById(R.id.base_string);
         try {
-            URL githubSearchUrl = new URL("https://jsonplaceholder.typicode.com/posts/1");
-            //URL githubSearchUrl = new URL("http://api.androidhive.info/contacts/");
+            //URL githubSearchUrl = new URL("https://jsonplaceholder.typicode.com/");
+            URL githubSearchUrl = new URL("https://jsonplaceholder.typicode.com/posts");
+            //URL githubSearchUrl = new URL("http://api.androidhive.info/contacts/contacts/0/33");
             //URL githubSearchUrl = new URL("https://api.github.com/search/repositories?" +
             //        "q=hello&sort=stars");
             Log.e(TAG, "Response from the url:" + githubSearchUrl);
@@ -51,20 +61,30 @@ public class MainActivity extends AppCompatActivity {
             String githubSearchResults = null;
             try {
                 githubSearchResults = networkUtils.getResponseFromHttpUrl(searchUrl);
+                Log.e(TAG, "Running the program: Title " + githubSearchResults);
+
                 ///*
                 try {
-                    JSONObject jsonObj = new JSONObject(githubSearchResults);
-                    /*
-                    JSONArray posts = jsonObj.getJSONArray("posts");
-                    for (int i = 0; i< posts.length(); i++) {
-                        JSONObject temp = posts.getJSONObject(i);
-                        userid = temp.getInt("userId");
-                        title = temp.getString("title");
-                        Log.e(TAG, "Running the program: Title " + title);
+                    JSONArray jsonObj = new JSONArray(githubSearchResults);
+                    //JSONObject object = (JSONObject) new JSONTokener(githubSearchResults).nextValue();
+                    Log.e(TAG, "Running the program: Temp " + jsonObj);
+                    for(int i =0; i<jsonObj.length(); i++){
+                        Log.e(TAG, "Key value " + jsonObj.optJSONObject(i));
+                        JSONObject temp_temp = jsonObj.optJSONObject(i);
+
+                        int id = temp_temp.getInt("id");
+                        int userId = temp_temp.getInt("userId");
+                        String title = temp_temp.getString("title");
+
+                        HashMap<String, String> temp = new HashMap<>();
+                        temp.put("id", Integer.toString(id));
+                        temp.put("userid", Integer.toString(userId));
+                        temp.put("title", title);
+                        Log.e(TAG, "temp temp this" + temp);
+                        database.add(temp);
+                        Log.e(TAG, "Adding to database"+ temp);
                     }
-                    */
-                    userid = jsonObj.getInt("userId");
-                    title = jsonObj.getString("title");
+
                 }
                 catch (final JSONException e){
                     Log.e(TAG, "JSON parsing error:" + e.getMessage());
@@ -74,13 +94,22 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return githubSearchResults;
+            return "string";
         }
 
         @Override
         protected void onPostExecute(String githubSearchResults) {
+            String test = null;
             if (githubSearchResults != null && !githubSearchResults.equals("")) {
-                mSearchResultsTextView.setText(title);
+                for(int i = 0; i<database.size(); i++){
+                    HashMap<String, String> temp = new HashMap<>();
+                    temp = database.get(i);
+                    for(String key: temp.keySet()){
+                        test += key + "-> " + temp.get(key) + "\n";
+                    }
+                }
+                mSearchResultsTextView.setText(test);
+
             }
         }
     }
