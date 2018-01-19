ad9efe9793bd8ed12613a915b1897663beb32533
Pritish Yuvraj <pritish.yuvi@gmail.com>
2018-01-19 02:17:21 -0500

    bugs fixed

--- Priority Task/app/src/main/java/com/example/pritish/prioritytask/MainActivity.java
+++ Priority Task/app/src/main/java/com/example/pritish/prioritytask/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.pritish.prioritytask;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    TaskDBHelper db = new TaskDBHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        TaskDBHelper db = new TaskDBHelper(this);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,8 +41,39 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        display();
     }
+    public void display(){
+        //Parent Layout
+        LinearLayout parentLayout = (LinearLayout) findViewById(R.id.layout);
 
+
+        //Layout Inflater
+        LayoutInflater layoutInflater = getLayoutInflater();
+
+        View view;
+
+        Cursor cursor = db.get_task_description_priority();
+        if (cursor.moveToFirst()){
+            do{
+                String desc = cursor.getString(1);
+                String priority = cursor.getString(2);
+                //TextView temp = findViewById(R.id.temp);
+                //temp.setText(desc);
+                try {
+                    //view = layoutInflater.inflate(R.layout.text_view, parentLayout, false);
+                    View myLayout = layoutInflater.inflate(R.layout.text_view, parentLayout, false);
+                    TextView textView = (TextView) myLayout.findViewById(R.id.text_);
+                    TextView textView1 = (TextView)myLayout.findViewById(R.id.priority);
+                    textView1.setText(priority);
+                    textView.setText(desc);
+                    parentLayout.addView(myLayout);
+                }catch (Exception e){
+                    Log.e("tag", desc + "exception "+e.toString());
+                }
+            }while (cursor.moveToNext());
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
