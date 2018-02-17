37af949e35efe1c7ab87cc31a9033f7e45ee601a
Pritish Yuvraj <pritish.yuvi@gmail.com>
2018-02-17 12:08:56 -0500

    android python intergration

--- integrate_android_python/todo-api/app.py
+++ integrate_android_python/todo-api/app.py
@@ -1,6 +1,9 @@
 #!flask/bin/python
 
 from flask import Flask, jsonify
+from flask import abort 
+from flask import make_response 
+from flask import request
 
 app = Flask(__name__)
 
@@ -23,5 +26,68 @@ tasks = [
 def get_tasks():
 	return jsonify({'tasks':tasks})
 
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+def get_task(task_id):
+	task = [task for task in tasks if task['id'] == task_id]
+	if len(task) == 0:
+		abort(404)
+
+	return jsonify({'task':task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'error':'Not found'}), 404)
+
+@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
+def create_task():
+	if not request.json or not 'title' in request.json:
+		abort(400)
+	task = {
+	'id':tasks[-1]['id'] + 1,
+	'title':request.json['title'],
+	'description':request.json.get('description', ""),
+	'done':False
+	}
+	tasks.append(task)
+	#print tasks
+	return jsonify({'task':task}), 201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
 if __name__ == '__main__':
 	app.run(debug = True)
+
+
+	#Commands for transmitting data using RESTful API
+	# curl -i -H "Content-Type: application/json" -X POST -d '{"title": "Read a book"}' http://localhost:5000/todo/api/v1.0/tasks
+	# curl -i http://localhost:5000/todo/api/v1.0/tasks/1
+	# curl -i http://localhost:5000/todo/api/v1.0/tasks
+	# curl -i -H "Content-Type: application/json" -X PUT -d '{"done":true}' http://localhost:5000/todo/api/v1.0/tasks/2
+	# curl -i -H "Content-Type: application/json" -X DELETE  http://localhost:5000/todo/api/v1.0/tasks/2
